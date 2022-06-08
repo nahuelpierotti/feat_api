ac848c10595d0e541c920411bc7285f8b0532425
Nahuel Pierotti <npie@unlam.edu.ar>
2022-06-08 00:58:45 -0300

    Corrige endpoint de sport quitando generics. Agrega endpoint de valuations

--- src/routes/sports.routes.ts
+++ src/routes/sports.routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import {
   findAll,
-  findGeneric,
   findOne
 } from "../controllers/sport.controller";
 
@@ -10,8 +9,6 @@ const router = Router();
 router.route("/").get(findAll);
 
 router.route("/:id").get(findOne);
-
-router.route("/getGenerics/").get(findGeneric);
 
 export default router;
 
