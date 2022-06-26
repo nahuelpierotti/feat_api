80f11ac87820b613694329a70e26482f53537f4e
Nahuel Pierotti <npie@unlam.edu.ar>
2022-06-25 21:23:41 -0300

    Agrega control de envio de solicitudes y aceptacion de solicitudes

--- src/controllers/eventApply.controller.ts
+++ src/controllers/eventApply.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { createQueryBuilder } from "typeorm";
+import { createQueryBuilder, getRepository } from "typeorm";
 import { EventApply } from "../models/EventApply";
 import { Player } from "../models/Player";
 import { Sport } from "../models/Sport";
@@ -14,33 +14,34 @@ export const create = async (req: Request, res: Response) => {
       const eventId= req.body.eventId
       const originId= req.body.origin
 
-      /*const playerByUser=  await
+      const existe=  await
       createQueryBuilder()
-      .select("player.id")
-      .from(Player, "player")
-      .innerJoin("player.person", "person")
-      .innerJoin(SportGeneric,"sportGeneric","player.sport=sportGeneric.id")
-      .innerJoin(Sport,"sport", "sportGeneric.id=sport.sportGenericId")
-      .innerJoin(Event,"event","sport.id=event.sportId")
-      .where("player.id = :playerId", {playerId })
-      .andWhere("event.id= :eventId",{eventId})
-      .getOneOrFail()
-*/
-      const event_apply= await
-      createQueryBuilder()
-      .insert()
-      .into(EventApply)
-      .values({
-          origin: originId,
-          state: + 6,
-          event: + eventId,
-          player: + playerId,    
-          date: () => 'CURRENT_TIMESTAMP'
-        }).execute()
-      
-      //  console.log(event_apply)
+      .select("eventApply.id")
+      .from(EventApply, "eventApply")
+      .where("eventApply.playerId = :playerId", {playerId })
+      .andWhere("eventApply.eventId= :eventId",{eventId})
+      .getOne()
 
-      res.status(200).json("Solicitud Enviada Exitosamente!");
+      if(existe!=undefined){
+
+        const event_apply= await
+        createQueryBuilder()
+        .insert()
+        .into(EventApply)
+        .values({
+            origin: originId,
+            state: + 6,
+            event: + eventId,
+            player: + playerId,    
+            date: () => 'CURRENT_TIMESTAMP'
+          }).execute()
+        
+        //  console.log(event_apply)
+
+        res.status(200).json("Solicitud Enviada Exitosamente!");
+      }else{
+        res.status(200).json("La Solicitud ya habia sido enviada Exitosamente!");
+      }
   
     }catch (error) {
       console.log(error);
@@ -111,46 +112,59 @@ export const create = async (req: Request, res: Response) => {
       const playerId = req.body.playerId
       const eventId = req.body.eventId
       
-      const event_apply=  await
+      const existe=  await
       createQueryBuilder()
-      .select("eventApply")
-      //.addSelect("player.id")
+      .select("eventApply.id")
       .from(EventApply, "eventApply")
-      /*.innerJoin("eventApply.player","player")
-      .innerJoin("player.person", "person")*/
       .where("eventApply.playerId = :playerId", {playerId })
       .andWhere("eventApply.eventId= :eventId",{eventId})
-      .getOneOrFail()
+      .getOne()
 
-      const applyUpd= await
-      createQueryBuilder()
-      .update(EventApply)
-      .set({
-        state: 7
-
-      })
-      /*.where("event = :eventId", { eventId})
-      .andWhere("player = :playerId",{playerId: event_apply.player})*/
-      .where("id= :id",{id: event_apply.id})
-      .execute()
-
-      console.log(applyUpd)
-
-      const accept_apply= await
-      createQueryBuilder()
-      .insert()
-      .into(PlayerList)
-      .values({
-          origin: event_apply.origin,
-          state: 9,
-          event: + eventId,
-          player: playerId,    
-          date: () => 'CURRENT_TIMESTAMP'
-        }).execute()
+      if(existe!=undefined){
       
-        console.log(accept_apply)
+        const event_apply=  await
+        createQueryBuilder()
+        .select("eventApply")
+        //.addSelect("player.id")
+        .from(EventApply, "eventApply")
+        /*.innerJoin("eventApply.player","player")
+        .innerJoin("player.person", "person")*/
+        .where("eventApply.playerId = :playerId", {playerId })
+        .andWhere("eventApply.eventId= :eventId",{eventId})
+        .getOneOrFail()
 
-      res.status(200).json("Invitacion Aceptada Exitosamente!");
+        const applyUpd= await
+        createQueryBuilder()
+        .update(EventApply)
+        .set({
+          state: 7
+
+        })
+        /*.where("event = :eventId", { eventId})
+        .andWhere("player = :playerId",{playerId: event_apply.player})*/
+        .where("id= :id",{id: event_apply.id})
+        .execute()
+      
+        console.log(applyUpd)
+
+        const accept_apply= await
+        createQueryBuilder()
+        .insert()
+        .into(PlayerList)
+        .values({
+            origin: event_apply.origin,
+            state: 9,
+            event: + eventId,
+            player: playerId,    
+            date: () => 'CURRENT_TIMESTAMP'
+          }).execute()
+        
+          console.log(accept_apply)
+
+        res.status(200).json("Invitacion Aceptada Exitosamente!");
+      }else{
+        res.status(200).json("La Invitacion ya habia sido aceptada anteriormente!");
+      }
   
     }catch (error) {
       console.log(error);
@@ -195,6 +209,46 @@ export const create = async (req: Request, res: Response) => {
 
       res.status(200).json("Invitacion Rechazada Exitosamente!!");
   
+    }catch (error) {
+      console.log(error);
+      res.status(400).json(error);
+    }
+  };
+
+  export const exists = async (req: Request, res: Response) => {
+    try{
+      const playerId= req.params.playerId
+      const eventId= req.params.eventId
+      //const originId= req.body.origin
+
+      const existe=  await
+      createQueryBuilder()
+      .select("eventApply.id")
+      .from(EventApply, "eventApply")
+      .where("eventApply.playerId = :playerId", {playerId })
+      .andWhere("eventApply.eventId= :eventId",{eventId})
+      .getOne()
+
+      if(existe==undefined){
+        res.status(200).json("0");
+      }else{
+/*
+      const event_apply= await
+      createQueryBuilder()
+      .insert()
+      .into(EventApply)
+      .values({
+          origin: originId,
+          state: + 6,
+          event: + eventId,
+          player: + playerId,    
+          date: () => 'CURRENT_TIMESTAMP'
+        }).execute()
+      
+        console.log(event_apply)*/
+
+      res.status(200).json(existe);
+      }
     }catch (error) {
       console.log(error);
       res.status(400).json(error);
